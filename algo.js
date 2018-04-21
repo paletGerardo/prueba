0995e1ed24068ffb4255bf0c9f216e7a335f0986
gera <paletgerardo@gmail.com>
2018-04-21 00:13:02 -0300

    primer comentario

---
+++ algo.js
@@ -0,0 +1,4 @@
+function init() {
+    var un = "un string";
+    alert(un + "hola");
+}
---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gera
+ * Date: 20/04/18
+ * Time: 23:08
+ */
+echo "algo mas";
